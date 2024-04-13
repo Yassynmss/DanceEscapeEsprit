90c432cafc214f1ad13da84ae7cdca4ad4290b2f
YassynMss <yassinetlili97@gmail.com>
2024-04-13 18:42:01 +0100

    hhhh

--- src/app/BackOffice/alltemplateback/alltemplateback.component.ts
+++ src/app/BackOffice/alltemplateback/alltemplateback.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
 
+import { UserService } from 'src/app/services/user.service';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-alltemplateback',
   templateUrl: './alltemplateback.component.html',
   styleUrls: ['./alltemplateback.component.css']
 })
-export class AlltemplatebackComponent {
+export class AlltemplatebackComponent implements OnInit{
+  chart: any;
+  rolesData: any[] = [];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.fetchRolesStats();
+  }
+
+  fetchRolesStats(): void {
+    this.userService.getRolesStats().subscribe(data => {
+      this.rolesData = data;
+      this.createChart();
+    });
+  }
+
+  createChart(): void {
+    const ctx = document.getElementById('rolesChart') as HTMLCanvasElement;
+    this.chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: this.rolesData.map(role => role[0]),
+        datasets: [{
+          label: 'Roles',
+          data: this.rolesData.map(role => role[1]),
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
--- src/app/BackOffice/sidebarback/sidebarback.component.ts
+++ src/app/BackOffice/sidebarback/sidebarback.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sidebarback',
   templateUrl: './sidebarback.component.html',
   styleUrls: ['./sidebarback.component.css']
 })
-export class SidebarbackComponent {
+export class SidebarbackComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn(); // Vérifiez si l'utilisateur est connecté
+    if (this.isLoggedIn) {
+    }
+  }
 
+  constructor(private authService: AuthService) { }
+  logout(): void {
+    this.authService.logout(); // Déconnectez l'utilisateur
+    this.isLoggedIn = false; // Mettez à jour le statut de connexion
+  }
 }
