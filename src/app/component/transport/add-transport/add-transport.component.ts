504573ae3059efb67b7cef90ea9bc9c9971c3f57
aminelahmer1 <amineca999@gmail.com>
2024-04-26 11:20:47 +0200

    add map

--- src/app/component/transport/add-transport/add-transport.component.ts
+++ src/app/component/transport/add-transport/add-transport.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TransportService } from 'src/app/core/services/TransportService/transport-service.service';
 import { Transport } from 'src/app/core/models/transport/transport';
 import { Logistic } from 'src/app/core/models/logistic/logistic';
 import { LogisticService } from 'src/app/core/services/LogisticService/logistic-service.service';
-
+import * as L from 'leaflet';
+import 'leaflet-search';
 @Component({
   selector: 'app-add-transport',
   templateUrl: './add-transport.component.html',
   styleUrls: ['./add-transport.component.css']
 })
-export class AddTransportComponent implements OnInit {
+export class AddTransportComponent implements OnInit, AfterViewInit {
   transportForm!: FormGroup; 
-
   logistics: Logistic[] = [];
+  map!: L.Map;
+  startMarker!: L.Marker;
+  endMarker!: L.Marker;
+  thirdMarker!: L.Marker;
+
   constructor(private transportService: TransportService, private formBuilder: FormBuilder, private logisticService: LogisticService) { }
 
   ngOnInit(): void {
-
     this.logisticService.getAllLogistics().subscribe(logistics => {
       this.logistics = logistics;
     });
@@ -25,11 +29,44 @@ export class AddTransportComponent implements OnInit {
     this.transportForm = this.formBuilder.group({
       route: ['', Validators.required],
       description: ['', Validators.required],
-       startLocation_longitude: ['', Validators.required],
+      startLocation_longitude: ['', Validators.required],
       startLocation_latitude: ['', Validators.required],
       endLocation_longitude: ['', Validators.required],
       endLocation_latitude: ['', Validators.required],
       id_logistic: ['', Validators.required]
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  initMap(): void {
+
+    
+    this.map = L.map('map').setView([34.0000, 9.0000], 8); // Centrer la carte sur la Tunisie
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+    }).addTo(this.map);
+
+    this.map.on('click', (e) => {
+      if (!this.startMarker) {
+        this.startMarker = L.marker(e.latlng).addTo(this.map);
+        this.transportForm.patchValue({
+          startLocation_latitude: e.latlng.lat,
+          startLocation_longitude: e.latlng.lng
+        });
+      } else if (!this.endMarker) {
+        this.endMarker = L.marker(e.latlng).addTo(this.map);
+        this.transportForm.patchValue({
+          endLocation_latitude: e.latlng.lat,
+          endLocation_longitude: e.latlng.lng
+        });
+      } else if (!this.thirdMarker) {
+        this.thirdMarker = L.marker(e.latlng).addTo(this.map);
+        // Vous pouvez ajouter des actions supplémentaires ici
+      }
     });
   }
 
--- src/app/component/transport/list-transport/list-transport.component.ts
+++ src/app/component/transport/list-transport/list-transport.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { TransportService } from 'src/app/core/services/TransportService/transport-service.service';
 import { Transport } from 'src/app/core/models/transport/transport';
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+import 'leaflet-control-geocoder';
 
 @Component({
   selector: 'app-list-transport',
   templateUrl: './list-transport.component.html',
   styleUrls: ['./list-transport.component.css']
 })
-export class ListTransportComponent implements OnInit {
+export class ListTransportComponent implements OnInit, AfterViewInit {
   transports: Transport[] = [];
 
   constructor(private transportService: TransportService) { }
@@ -16,15 +19,42 @@ export class ListTransportComponent implements OnInit {
     this.retrieveTransports();
   }
 
+  ngAfterViewInit(): void {
+    this.initializeMaps();
+  }
+
   retrieveTransports(): void {
     this.transportService.getAllTransports()
       .subscribe(
         data => {
           this.transports = data;
           console.log(data);
+          this.initializeMaps(); // Initialize maps after retrieving transports
         },
         error => console.error(error)
       );
+  }
+
+  initializeMaps(): void {
+    this.transports.forEach(transport => {
+      const map = L.map(`map-${transport.id_transport}`).setView(
+        [parseFloat(transport.startLocation_latitude), parseFloat(transport.startLocation_longitude)],
+        13
+      );
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      }).addTo(map);
+
+      L.Routing.control({
+        waypoints: [
+          L.latLng(parseFloat(transport.startLocation_latitude), parseFloat(transport.startLocation_longitude)),
+          L.latLng(parseFloat(transport.endLocation_latitude), parseFloat(transport.endLocation_longitude))
+        ],
+        routeWhileDragging: true
+      }).addTo(map);
+    });
   }
 
   removeTransport(transport: Transport): void {
